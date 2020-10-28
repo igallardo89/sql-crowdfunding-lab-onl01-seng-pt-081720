e9e9b1f5c48a715b05b344d46c49f4219a560690
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 19:51:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,5 +53,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.category, SUM(pledges.amount)
    FROM projects
    INNER JOIN pledges
-   "
+   WHERE pledges.project_id = projects.id AND projects.category = 'books'
+   GROUP BY projects.category;"
 end
