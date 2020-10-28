b9935f900c683126f137152241c42b0b0d7b1f4f
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 19:49:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT project.category, pledges.amount
   FROM projects
   INNER JOIN pledges
-  WHERE pledges.project_id = project.id AND projects.category = 'music';"
+  WHERE pledges.project_id = projects.id AND projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
